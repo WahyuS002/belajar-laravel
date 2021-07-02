78aa5a7a34842266af9caada23917d81d66ce364
WahyuS002 <syahputrawahyu61@gmail.com>
2021-07-03 06:09:27 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -35,10 +35,13 @@ Auth::routes();
 */
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/post', 'PostController@index')->name('post');
-Route::get('/post/create', 'PostController@create')->name('post.create');
-Route::post('/post/store', 'PostController@store')->name('post.store');
-Route::get('/post/edit/{post}', 'PostController@edit')->name('post.edit');
-Route::patch('/post/update/{post}', 'PostController@update')->name('post.update');
-Route::delete('/post/destroy/{post}', 'PostController@destroy')->name('post.destroy');
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/post', 'PostController@index')->name('post');
+    Route::get('/post/create', 'PostController@create')->name('post.create');
+    Route::post('/post/store', 'PostController@store')->name('post.store');
+    Route::get('/post/edit/{post}', 'PostController@edit')->name('post.edit');
+    Route::patch('/post/update/{post}', 'PostController@update')->name('post.update');
+    Route::delete('/post/destroy/{post}', 'PostController@destroy')->name('post.destroy');
+});
+
 
